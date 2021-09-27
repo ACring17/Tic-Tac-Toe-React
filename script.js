5ff4cd50b3d7c08e3645457185c99704ec45d643
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-09-27 11:26:09 -0700

    Update script.js
    Adjusted the square class and added a function for the board to handle the state of the board on click.

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 class Square extends React.Component {
-   constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-  
   render() {
     return (
       <button 
        className="square" 
-       onCLick={() => this.setState({value: 'X'})}
-   >
-        {this.state.value}
+       onCLick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
@@ -24,6 +17,12 @@ class Board extends React.Component {
         this.state = {
             squares: Array(9).fill(null),
         };
+    }
+   
+   handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
 
     renderSquare(i) {
