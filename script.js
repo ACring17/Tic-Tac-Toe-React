60d3038ac8138a13f98d41dd32adc717ae8259a0
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-10-01 15:01:58 -0700

    Update script.js
    Added a key for the history buttons for the user to time travel

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ class Game extends React.Component {
                 'Go to move #' + move :
                 'GO to game start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
